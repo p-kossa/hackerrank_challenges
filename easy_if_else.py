2b9db7aa76283e95af034cd41f62339e71b05b50
Kevin Ossa <kevin.ossa@myquestis.com>
2018-11-29 11:17:11 -0500

    couple of easy challenges

---
+++ easy_if_else.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+num = int(input())
+
+if (num % 2 == 0) and (2 <= num <= 5):
+    print("Not Weird")
+elif (num % 2 == 0) and (6 <= num <= 20):
+    print("Weird")
+elif (num % 2 == 0) and num > 20:
+    print("Not Weird")
+else:
+    print("Weird")
---
+++ leap_year.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def is_leapyear(year: int) -> None:
+    if year % 4 == 0:
+        pass
+
+
+year = int(input())
+print(is_leapyear(year))
